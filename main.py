5f62376663d21d4a10645734b0afc78be4b56a37
Emma Paz <emma@developmentseed.org>
2023-06-24 18:24:27 -0400

    add langchain functionality

--- main.py
+++ main.py
@@ -33,31 +33,25 @@ async def tour(
     distance: Annotated[int, Form()],
     start_time: Annotated[str, Form()],
 ):
-    # location = "Philadelphia, PA"
-    return location, interests, budget, duration, distance, start_time
-    # chat_prompt = f"""
-    #     You are an expert in planning walking tours around {location}.
-    #     """
-    # system_prompt = SystemMessagePromptTemplate.from_template(chat_prompt)
+    chat_prompt = f"""
+        You are an expert in planning walking tours around {location}.
+        """
+    system_prompt = SystemMessagePromptTemplate.from_template(chat_prompt)
 
-    # if len(interests) > 1:
-    #     interests = f"""{", ".join(interests[:-1])} and {interests[-1]}"""
+    interests = ", ".join(interests)
+    if len(interests) > 1:
+        interests = f"""{", ".join(interests[:-1])} and {interests[-1]}"""
 
-    # # check if any of the parameters are empty
-    # if not interests or not budget or not duration or not distance or not time_of_day:
-    #     # return error
-    #     return "Please fill out all fields"
+    human_template = f"""
+        I am interested in {interests}. I have {duration} hours and would like to walk no more than {distance} miles. My budget is {budget} dollars. I want to start in the {start_time}. Please give me a list of locations for a {location} walking tour based on the previous parameters. With each location, provide a recommended start time recommended time at the location, a category, a fun, detailed story about the location, and its geolocation. Present each location as an enthusiastic tour guide. Format the results into a json response complete with location name, category, suggested_start_time, suggested_visit_duration, story and geolocation field.
+        """
+    # print("HUMAN ASK", human_template)
+    human_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    chat_prompt = ChatPromptTemplate.from_messages([system_prompt, human_prompt])
 
-    # human_template = f"""
-    #     I am interested in {interests}. I have {duration} hours and would like to walk no more than {distance} miles. My budget is {budget} dollars. I want to start in the {time_of_day}. Please give me a list of locations for a {location} walking tour based on the previous parameters. With each location, provide a recommended start time recommended time at the location, a category, a fun, detailed story about the location, and its geolocation. Present each location as an enthusiastic tour guide. Format the results into a json response complete with location name, category, suggested_start_time, suggested_visit_duration, story and geolocation field.
-    #     """
-    # # print("HUMAN ASK", human_template)
-    # human_prompt = HumanMessagePromptTemplate.from_template(human_template)
-    # chat_prompt = ChatPromptTemplate.from_messages([system_prompt, human_prompt])
+    chain = LLMChain(llm=chat, prompt=chat_prompt)
+    response = chain.run(
+        input_language="English", output_language="English", max_tokens=100
+    )
 
-    # chain = LLMChain(llm=chat, prompt=chat_prompt)
-    # response = chain.run(
-    #     input_language="English", output_language="English", max_tokens=100
-    # )
-
-    # return {response}
+    return {response}
